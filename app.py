def7fcc12810bd5723befd150d94a01ed93b7546
Bhushan Zade <bhushanzade.in@gmail.com>
2024-10-24 19:13:53 +0530

    app.py

--- app.py
+++ app.py
@@ -3,54 +3,51 @@ import pickle
 import string
 from nltk.corpus import stopwords
 import nltk
-nltk.download('punkt')
-nltk.download('stopwords')
 from nltk.stem.porter import PorterStemmer
 
-ps = PorterStemmer()
+# Download NLTK resources
+nltk.download('punkt')
+nltk.download('stopwords')
 
+# Initialize the stemmer
+ps = PorterStemmer()
+stop_words = set(stopwords.words('english'))  # Store stopwords in a set for efficiency
 
 def transform_text(text):
     text = text.lower()
     text = nltk.word_tokenize(text)
 
-    y = []
-    for i in text: 
-        if i.isalnum():
-            y.append(i)
+    # Filter out non-alphanumeric characters
+    y = [i for i in text if i.isalnum()]
 
-    text = y[:]
-    y.clear()
+    # Remove stopwords and punctuation
+    y = [i for i in y if i not in stop_words and i not in string.punctuation]
 
-    for i in text:
-        if i not in stopwords.words('english') and i not in string.punctuation:
-            y.append(i)
-
-    text = y[:]
-    y.clear()
-
-    for i in text:
-        y.append(ps.stem(i))
+    # Stemming
+    y = [ps.stem(i) for i in y]
 
     return " ".join(y)
 
-tfidf = pickle.load(open('vectorizer1.pkl','rb'))
-model = pickle.load(open('model.pkl','rb'))
+# Load the TF-IDF vectorizer and model
+tfidf = pickle.load(open('vectorizer1.pkl', 'rb'))
+model = pickle.load(open('model.pkl', 'rb'))
 
-st.title("Sentiment Analysis-Movies Review")
+st.title("Sentiment Analysis - Movies Review")
 
 input_sms = st.text_area("Enter the message")
 
 if st.button('Predict'):
-
-    # 1. preprocess
-    transformed_sms = transform_text(input_sms)
-    # 2. vectorize
-    vector_input = tfidf.transform([transformed_sms])
-    # 3. predict
-    result = model.predict(vector_input)[0]
-    # 4. Display
-    if result == 1:
-        st.header("Positive Sentiment")
+    if input_sms:  # Check if there is input
+        # 1. Preprocess
+        transformed_sms = transform_text(input_sms)
+        # 2. Vectorize
+        vector_input = tfidf.transform([transformed_sms])
+        # 3. Predict
+        result = model.predict(vector_input)[0]
+        # 4. Display
+        if result == 1:
+            st.header("Positive Sentiment")
+        else:
+            st.header("Negative Sentiment")  # Corrected typo
     else:
-        st.header("Neagtive Sentiment")
+        st.warning("Please enter a message to analyze.")
